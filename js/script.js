594434e0787329147a3cff3bdcf2b717b3ba8b64
gsdlf <gsdlf@yahoo.es>
2018-12-30 20:28:40 +0100

    Funcionamiento menú

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var anchoPantalla,
 	altoPantalla;
 var anchoMovil=false;
 var cuadricula;
+var scrollMovil;
+var altoThumbMovil='30vw'; //56vw
+var menuAbierto=true;
+var zoomMenu=false;
 
 function empezar(){
 	cuadricula=Math.sqrt(nThumbs);
@@ -24,11 +28,16 @@ function resize() {
 
 	if (anchoMovil==true){
 		$('.thumb').css('width','100vw');
-		$('.thumb').css('height','56vw');
+		$('.thumb').css('height',altoThumbMovil);
 	}else{
 		$('.thumb').css('width',100/cuadricula+'%');
 		$('.thumb').css('height',100/cuadricula+'%');
 	}
+
+	//console.log($('#x_cerrar').height());
+
+	//$('.contenido').css('height',($('#pagina').height()-$('#titulo_0').height())+'px');
+	//$('.contenido').css('top',($('.xcerrar.brillo').height())+'7vw');
 }
 window.onresize = resize;
 
@@ -42,6 +51,7 @@ function crearThumbs(){
 	  var texto=$( '.titulo',this ).html();
 	  $(this).append('<div class="titulo brillo">'+texto+'</div>');
 	  $(this).append('<div class="titulo">'+texto+'</div>');
+	  //$(this).append('<div class="contenido"></div>');
 	});
 	var textoX=$('.xcerrar').html();
 	$('#x_cerrar').append('<div class="xcerrar brillo">'+textoX+'</div>');
@@ -68,46 +78,84 @@ function crearSombra(){
 }
 
 function clickThumb(n){
-	$('#pagina').css('width',(100*cuadricula)+'vw');
-	$('#pagina').css('height',($('#pagina').height()*3)+'px');
+	if (anchoMovil==true){
+		$('.thumb').css('height','95vh');
+		var posicionY=Math.round($('#thumb_'+n).offset().top/$('#thumb_'+n).height());
+		//$('#pagina').css('top',(($('#pagina').height()*posicionY-$('#cabecera').outerHeight())*-1)+'px');
+		scrollMovil=$("html, body").scrollTop();
+		$("html, body").animate({ scrollTop: (($('#pagina').height()*posicionY)) }, 500);
+	}else{
+		$('#pagina').css('width',(100*cuadricula)+'vw');
+		$('#pagina').css('height',($('#pagina').height()*3)+'px');
 
-	$('#contenedor').css('overflow','hidden');
+		$('#contenedor').css('overflow','hidden');
 
-	var y=$('#thumb_'+n).offset().top-$('#cabecera').outerHeight();
-	var x=$('#thumb_'+n).offset().left;
+		var y=$('#thumb_'+n).offset().top-$('#cabecera').outerHeight();
+		var x=$('#thumb_'+n).offset().left;
 
-	var posicionY=Math.round($('#thumb_'+n).offset().top/$('#thumb_'+n).height());
-	var posicionX=Math.round($('#thumb_'+n).offset().left/$('#thumb_'+n).width());
+		var posicionY=Math.round($('#thumb_'+n).offset().top/$('#thumb_'+n).height());
+		var posicionX=Math.round($('#thumb_'+n).offset().left/$('#thumb_'+n).width());
 
-	$('#pagina').css('top',(($('#pagina').height()*posicionY-$('#cabecera').outerHeight())*-1)+'px');
-	$('#pagina').css('left',($('#pagina').width()*posicionX*-1)+'px');
+		$('#pagina').css('top',(($('#pagina').height()*posicionY-$('#cabecera').outerHeight())*-1)+'px');
+		$('#pagina').css('left',($('#pagina').width()*posicionX*-1)+'px');		
+	}
+
+	$('#thumb_'+n+' .gradiente_blanco').css('opacity','0.7');
 
 	$('#x_cerrar').css('display','block');
 	$('#x_cerrar').css('opacity','1');
-	//$('#x_cerrar').fadeIn(500);
-}
 
-/*function thumbMouseover(n){
-	$('#thumb_'+n+' .gradiente_blanco').css('opacity','0.7');
+	zoomMenu=true;
 }
-
-function thumbMouseout(n){
-	$('#thumb_'+n+' .gradiente_blanco').css('opacity','0.3');
-}*/
 
 function cerrar(){
-	$('#pagina').css('width','');
-	$('#pagina').css('height','');
+	if (anchoMovil==true){
+		$('.thumb').css('height',altoThumbMovil);
+		$("html, body").animate({ scrollTop: scrollMovil }, 500);
+	}else{
+		$('#pagina').css('width','100vw');
+		$('#pagina').css('height','95vh');
 
-	$('#contenedor').css('overflow','auto');
+		$('#contenedor').css('overflow','auto');
 
-	$('#pagina').css('top','');
-	$('#pagina').css('left','');
+		$('#pagina').css('top','5vh');
+		$('#pagina').css('left','0');		
+	}
+
+	$('.thumb .gradiente_blanco').css('opacity','0.3');
 
 	$('#x_cerrar').css('opacity','0');
 
 	setTimeout(function(){
 		$('#x_cerrar').css('display','none');
 	},500);
+
+	zoomMenu=false;
+}
+
+function menu(){
+	console.log(menuAbierto,zoomMenu);
+	if (menuAbierto==true){
+		if (zoomMenu==true){
+			cerrar();
+			setTimeout(menu,500);
+		}else{
+			if (anchoMovil==true){
+				$('#pagina').css('left',-anchoPantalla+'px');
+			}else{
+				$('#pagina').css('top',-altoPantalla+'px');
+			}
+			menuAbierto=false;
+		}		
+	}else{
+		if (anchoMovil==true){
+			$("html, body").animate({ scrollTop: 0 }, 500);
+			$('#pagina').css('left','0');
+		}else{
+			$('#pagina').css('top','5vh');
+		}
+		menuAbierto=true;
+	}
+	
 }
 
